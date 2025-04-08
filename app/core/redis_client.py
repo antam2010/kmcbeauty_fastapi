59495a8d90f83a5e9ba57b4519c14e1f311a9c44
antam2010 <antam2010@naver.com>
2025-04-08 19:12:36 +0900

    디버그 환경에 따라 Redis 클라이언트 호스트를 동적으로 설정하도록 수정

--- app/core/redis_client.py
+++ app/core/redis_client.py
@@ -1,3 +1,7 @@
 import redis
+from app.core.config import APP_ENV
 
-redis_client = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+if APP_ENV == "debug":
+    redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+else:
+    redis_client = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
