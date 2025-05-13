f1916f737049b426072e84d8903edfb5d1d3ef5e
antam2010 <antam2010@naver.com>
2025-05-13 23:51:08 +0900

    refresh_token 쿠키의 httponly 속성을 False로 변경하고 samesite 속성을 "None"으로 설정

--- app/api/auth.py
+++ app/api/auth.py
@@ -43,9 +43,9 @@ def login(
     response.set_cookie(
         key="refresh_token",
         value=refresh_token,
-        httponly=True,
+        httponly=False,
         secure=False,
-        samesite="Lux",
+        samesite="None",
         max_age=max_age,
     )
     return response
