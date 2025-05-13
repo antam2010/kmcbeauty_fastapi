ec5417d9d7f3dd2c6b243391060fc4cc575dc961
antam2010 <antam2010@naver.com>
2025-05-13 12:55:09 +0900

    oauth2_scheme의 토큰 URL을 사용되지 않는 값으로 변경

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -12,7 +12,9 @@ from app.exceptions import CustomException
 from app.models.user import User
 from app.utils.redis.user import get_user_redis, set_user_redis
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="not-used")
+
 
 
 def get_current_user(
